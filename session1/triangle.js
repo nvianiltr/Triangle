2eedb850f3b9874b8a5c8534e95497de2f55f6a6
Noviani Litriani <nvianiltr@yahoo.com>
2018-03-07 19:49:58 +0700

    add 2 more triangles and change the color of triangles

--- session1/triangle.js
+++ session1/triangle.js
@@ -13,9 +13,21 @@ window.onload = function init()
     // Three Vertices
     
     var vertices = [
-        vec2( -1, -1 ),
-        vec2(  0,  1 ),
-        vec2(  1, -1 )
+        // vec2( -1, -1 ),
+        // vec2(  0,  1 ),
+        // vec2(  1, -1 ),
+    
+        -1,-1,
+        -0.6,-0.3,
+        -0.2,-1,
+
+        -0.6,-0.3,
+        -0.2, 0.4,
+        0.2, -0.3,
+
+        -0.2, -1,
+        0.2, -0.3,
+        0.6,-1
     ];
 
     //
@@ -47,5 +59,5 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+    gl.drawArrays( gl.TRIANGLES, 0, 9 );
 }
